b0c4c23b7f12c53ce2715c1c68a3490f65ae7c51
RPSG1994 <rui-pedro-1994@hotmail.com>
2023-07-24 12:22:53 +0100

    Criar modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'colours', loadChildren: () => import('./colours/colours.module').then(m => m.ColoursModule) }, { path: 'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule) }, { path: 'genderAge', loadChildren: () => import('./gender-age/gender-age.module').then(m => m.GenderAgeModule) }, { path: 'material', loadChildren: () => import('./material/material.module').then(m => m.MaterialModule) }, { path: 'sizes', loadChildren: () => import('./sizes/sizes.module').then(m => m.SizesModule) }, { path: 'style', loadChildren: () => import('./style/style.module').then(m => m.StyleModule) }];
+const routes: Routes = [{ path: 'colours', loadChildren: () => import('./colours/colours.module').then(m => m.ColoursModule) }, { path: 'photos', loadChildren: () => import('./photos/photos.module').then(m => m.PhotosModule) }, { path: 'genderAge', loadChildren: () => import('./gender-age/gender-age.module').then(m => m.GenderAgeModule) }, { path: 'material', loadChildren: () => import('./material/material.module').then(m => m.MaterialModule) }, { path: 'sizes', loadChildren: () => import('./sizes/sizes.module').then(m => m.SizesModule) }, { path: 'style', loadChildren: () => import('./style/style.module').then(m => m.StyleModule) }, { path: 'brands', loadChildren: () => import('./brands/brands.module').then(m => m.BrandsModule) }, { path: 'productType', loadChildren: () => import('./product-type/product-type.module').then(m => m.ProductTypeModule) }, { path: 'shops', loadChildren: () => import('./shops/shops.module').then(m => m.ShopsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
