cc600a70831a38c455e637c3215477e7de5c14d9
RPSG1994 <rui-pedro-1994@hotmail.com>
2023-07-24 12:55:45 +0100

    Criar serviços

--- src/app/brands/brands.module.ts
+++ src/app/brands/brands.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { BrandsRoutingModule } from './brands-routing.module';
 import { BrandsComponent } from './brands.component';
 import { CreateBrandComponent } from './create-brand/create-brand.component';
