97991512db278389bcc1cb81ea7b99c66c9c8fee
Fagaio <140155600+Fagaio@users.noreply.github.com>
2023-07-24 13:20:54 +0100

    routing module style

--- src/app/style/style-routing.module.ts
+++ src/app/style/style-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StyleComponent } from './style.component';
+import { CreateStyleComponent } from './create-style/create-style.component';
+import { ViewStyleComponent } from './view-style/view-style.component';
+import { ViewAllStyleComponent } from './view-all-style/view-all-style.component'
 
-const routes: Routes = [{ path: '', component: StyleComponent }];
+const routes: Routes = [{ path: '', component: StyleComponent },
+{ path: 'create-style', component: CreateStyleComponent },
+{ path: 'view-style/:id', component: ViewStyleComponent },
+{ path: 'view-all-style', component: ViewAllStyleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
