1ed3b6c5845d4f16e6e36efc435072330b7c8583
Fagaio <140155600+Fagaio@users.noreply.github.com>
2023-07-24 14:51:18 +0100

    Modules routing Tabelas

--- src/app/sizes/sizes-routing.module.ts
+++ src/app/sizes/sizes-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SizesComponent } from './sizes.component';
+import { CreateSizeComponent } from './create-size/create-size.component';
+import { ViewSizeComponent } from './view-size/view-size.component';
+import { ViewAllSizesComponent } from './view-all-sizes/view-all-sizes.component'
 
-const routes: Routes = [{ path: '', component: SizesComponent }];
+const routes: Routes = [{ path: '', component: SizesComponent }
+  { path: 'create-size', component: CreateSizeComponent },
+  { path: 'view-size/:id', component: ViewSizeComponent },
+  { path: 'view-all-sizes', component: ViewAllSizesComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
