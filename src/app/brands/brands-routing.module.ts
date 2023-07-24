0b8eaffbd3afd16364b3b01890db3d3a0f88fac6
RPSG1994 <rui-pedro-1994@hotmail.com>
2023-07-24 17:49:28 +0100

    Adicionar Routes

--- src/app/brands/brands-routing.module.ts
+++ src/app/brands/brands-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrandsComponent } from './brands.component';
+import { CreateBrandComponent } from './create-brand/create-brand.component';
+import { ViewAllBrandsComponent } from './view-all-brands/view-all-brands.component';
+import { ViewBrandsComponent } from './view-brands/view-brands.component';
 
-const routes: Routes = [{ path: '', component: BrandsComponent }];
+const routes: Routes = [{ path: '', component: BrandsComponent },
+{path: 'create-brand', component: CreateBrandComponent},
+{path: 'view-brands', component: ViewBrandsComponent},
+{path: 'view-all-brands', component:ViewAllBrandsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
