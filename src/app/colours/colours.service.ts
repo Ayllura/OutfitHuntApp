d0ad067895fd639681aa209fc766c3297972d2d4
Ayllura <140155139+Ayllura@users.noreply.github.com>
2023-07-25 02:31:53 +0100

    create colour: service. html and component.ts

--- src/app/colours/colours.service.ts
+++ src/app/colours/colours.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Colours } from './colours';
 import { environment } from 'src/environments/environment.development';
 import { Observable } from 'rxjs';
@@ -7,7 +7,38 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class ColoursService {
 
-  constructor() { }
+export class ColoursService {
+  apiUrl: string = "";
+  constructor(private httpClient: HttpClient) {
+    this.apiUrl = environment.apiUrl + "Colours";
+  }
+
+  createColour(colourBody: any): Observable<Colours> {
+    console.log(colourBody);
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        // Adicione outros cabeçalhos, como autorização, se necessário.
+      })
+    };
+    return this.httpClient.post<Colours>(this.apiUrl, colourBody, httpOptions); //return an observable
+  }
+
+  updateColour(colourBody: any): Observable<Colours> {
+    return this.httpClient.put<Colours>(this.apiUrl, colourBody); //return an observable
+  }
+
+  deleteColour(id: number): Observable<Colours> {
+    return this.httpClient.delete<Colours>(this.apiUrl + "/" + id); //return an observable
+  }
+
+  getColour(id: number): Observable<Colours> {
+    return this.httpClient.get<Colours>(this.apiUrl + "/" + id); //return an observable
+  }
+
+  getAllColours(): Observable<Colours[]> {
+    return this.httpClient.get<Colours[]>(this.apiUrl); //return an observable
+  }
 }
--- src/app/colours/create-colour/create-colour.component.ts
+++ src/app/colours/create-colour/create-colour.component.ts
@@ -8,6 +8,21 @@ import { NgForm } from '@angular/forms';
   templateUrl: './create-colour.component.html',
   styleUrls: ['./create-colour.component.css']
 })
-export class CreateColourComponent {
+export class CreateColourComponent implements OnInit {
+  colourId = 0;
+  description = "";
 
+  constructor(private service: ColoursService) {
+  }
+  ngOnInit(): void {
+  }
+  createNewColour(form: NgForm) {
+    let colour = {
+      colourId: form.value.colourId,
+      description: form.value.description,
+    };
+    this.service.createColour(colour).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
