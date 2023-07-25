5500ae539a79b9a87b8d1bac05154da0623af44b
Fagaio <140155600+Fagaio@users.noreply.github.com>
2023-07-25 13:17:28 +0100

    Materials atualizado

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialService } from './material.service';
 import { Materials } from './material';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-material',
@@ -8,5 +9,24 @@ import { Materials } from './material';
   styleUrls: ['./material.component.css']
 })
 export class MaterialComponent {
-
+  materialId = "0";
+  description = ""
+  materialList: Materials[] = [] ;
+  
+  constructor(private service: MaterialService) {
+  }
+  ngOnInit(): void {
+    this.service.getAllMaterial().subscribe(data => {
+      this.materialList = data;
+      });
+  }
+  createNewMaterial(form: NgForm) {
+  let material = {
+  materialId: form.value.materialId,
+  description: form.value.description
+  };
+  this.service.createMaterial(material).subscribe(data => {
+  console.log(data);
+  });
+}
 }
