637bb91c74df093b6a18ae2e65fa42c44e44fcfb
Ayllura <140155139+Ayllura@users.noreply.github.com>
2023-07-25 05:13:40 +0100

    create photo and html

--- src/app/photos/create-photo/create-photo.component.ts
+++ src/app/photos/create-photo/create-photo.component.ts
@@ -8,6 +8,21 @@ import { NgForm } from '@angular/forms';
   templateUrl: './create-photo.component.html',
   styleUrls: ['./create-photo.component.css']
 })
-export class CreatePhotoComponent {
+export class CreatePhotoComponent implements OnInit {
+  photoId = 0;
+  link = "";
 
+  constructor(private service: PhotosService) {
+  }
+  ngOnInit(): void {
+  }
+  createNewPhoto(form: NgForm) {
+    let photo = {
+      photoId: form.value.photoId,
+      link: form.value.link,
+    };
+    this.service.createPhoto(photo).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
