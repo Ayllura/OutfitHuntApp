27f780c7c1b0789f4e20f9b1fb560b5c9e8ce9b1
RPSG1994 <rui-pedro-1994@hotmail.com>
2023-07-28 17:50:25 +0100

    Já está tudo a funcionar como dantes! YEY!

--- src/app/shops/shops.component.ts
+++ src/app/shops/shops.component.ts
@@ -1,9 +1,8 @@
-// Import required modules and classes
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ShopsService } from './shops.service';
 import { Shops } from './shops';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shops',
@@ -11,74 +10,67 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./shops.component.css']
 })
 export class ShopsComponent implements OnInit {
-  shopList: Shops[] = []; // An array to store the list of all shops
-  filteredShopList: Shops[] = []; // An array to store the filtered shops based on search criteria
-  newShop: Shops = { shopId: 0, name: '', link: '' }; // Create an instance of the Shops class for creating new shops
-  updateShopForm: FormGroup; // Form group for updating shop details
-  searchForm: FormGroup; // Form group for searching shops by name
+  shopList: Shops[] = [];
+  filteredShopList: Shops[] = [];
+  newShop: Shops = { shopId: 0, name: '', link: '' };
+  updateShopForm: FormGroup;
+  searchForm: FormGroup;
+
+  // Add a class-level variable to store the last clicked shopId
+  private lastClickedShopId: number | null = null;
 
   constructor(
-    private service: ShopsService, // Injecting the ShopsService to interact with the backend
-    private fb: FormBuilder, // Injecting FormBuilder for creating form groups
-    private router: Router // Injecting the Router to perform navigation
+    private service: ShopsService,
+    private fb: FormBuilder,
+    private router: Router
   ) {
-    // Setting up the updateShopForm with form controls and validation rules
     this.updateShopForm = this.fb.group({
       shopId: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
       name: ['', Validators.required],
       link: ['', Validators.required]
     });
 
-    // Setting up the searchForm with form controls and validation rules
+    // Initialize the search form
     this.searchForm = this.fb.group({
       name: ['', Validators.required]
     });
   }
 
   ngOnInit(): void {
-    // Lifecycle hook: Called after the component is constructed.
-    // Implement the logic to fetch the list of shops and populate 'shopList' here
     this.getShopList();
   }
 
   getShopList(): void {
-    // Method to fetch the list of all shops from the backend using the ShopsService
     this.service.getAllShops().subscribe(data => {
       this.shopList = data;
-      this.filteredShopList = [...this.shopList]; // Initializing the filteredShopList with the same data initially
+      // Initially, set filteredShopList to the complete shopList
+      this.filteredShopList = [...this.shopList];
     });
   }
 
   createNewShop(): void {
-    // Method to create a new shop with input validation and duplicate checks
-
-    // Checking if the new shop's ID, name, and link are valid
     if (this.newShop.shopId <= 0 || this.newShop.name === '' || this.newShop.link === '') {
       alert('Shop ID must be greater than 0, and Name and Link cannot be empty.');
       return;
     }
 
-    // Extracting the new shop's name and link for duplicate checks
     const newName = this.newShop.name;
     const newLink = this.newShop.link;
 
-    // Checking if a shop with the same name or link already exists (excluding the current shop being created)
-    const existingShopWithSameName = this.shopList.find(shop => shop.name === newName || shop.shopId !== this.newShop.shopId);
-    const existingShopWithSameLink = this.shopList.find(shop => shop.link === newLink || shop.shopId !== this.newShop.shopId);
+    const existingShopWithSameName = this.shopList.find(shop => shop.name === newName && shop.shopId !== this.newShop.shopId);
+    const existingShopWithSameLink = this.shopList.find(shop => shop.link === newLink && shop.shopId !== this.newShop.shopId);
 
     if (existingShopWithSameName || existingShopWithSameLink) {
-      alert('A shop with the same Name or Link already exists. Please choose a different Name or Link.');
+      alert('A shop with the same Name or Link already exists. Please choose different Name or Link.');
       return;
     }
 
-    // Creating the new shop using the ShopsService and adding it to the shopList
     this.service.createShop(this.newShop).subscribe(data => {
       this.shopList.push(data);
-      this.newShop = { shopId: 0, name: '', link: '' }; // Resetting the newShop object for the next creation
+      this.newShop = { shopId: 0, name: '', link: '' };
       alert('Shop created successfully.');
     });
   }
-
 
   updateShop() {
     // Method to update an existing shop with input validation and duplicate checks
@@ -128,9 +120,6 @@ export class ShopsComponent implements OnInit {
     );
   }
 
-  // A class-level variable to store the ID of the last clicked shop (used during shop deletion)
-  private lastClickedShopId: number | null = null;
-
   deleteShop(shopId: number): void {
     // Method to delete a shop with a confirmation check
 
@@ -141,60 +130,61 @@ export class ShopsComponent implements OnInit {
         this.service.deleteShop(shopId).subscribe(
           () => {
             alert('Shop deleted successfully');
-            // Removing the deleted shop from the shopList and updating the view
-            this.shopList = this.shopList.filter(shop => shop.shopId !== shopId);
-            // Reset the lastClickedShopId to null for the next deletion
-            this.lastClickedShopId = null;
-          },
-          error => {
-            alert('Error deleting Shop.');
-            // Reset the lastClickedShopId in case of an error to allow reconfirmation
-            this.lastClickedShopId = null;
-          }
-        );
+              // Removing the deleted shop from the shopList and updating the view
+              this.shopList = this.shopList.filter(shop => shop.shopId !== shopId);
+              // Reset the lastClickedShopId to null for the next deletion
+              this.lastClickedShopId = null;
+            },
+            error => {
+              alert('Error deleting Shop.');
+              // Reset the lastClickedShopId in case of an error to allow reconfirmation
+              this.lastClickedShopId = null;
+            }
+          );
+        } else {
+          // If the user cancels the deletion, reset the lastClickedShopId
+          this.lastClickedShopId = null;
+        }
       } else {
-        // If the user cancels the deletion, reset the lastClickedShopId
-        this.lastClickedShopId = null;
+        // If the user clicked a different delete button, set the lastClickedShopId to the current shopId
+        this.lastClickedShopId = shopId;
+        if (confirm('Are you sure you want to delete this Shop?')) {
+          // Do not proceed with deletion at this point
+          return;
+        }
       }
-    } else {
-      // If the user clicked a different delete button, set the lastClickedShopId to the current shopId
-      this.lastClickedShopId = shopId;
-      if (confirm('Are you sure you want to delete this Shop?')) {
+    }
+
+    searchByName(): void {
+      // Method to search for shops by name and display the results.
+
+      // Check if the search form is invalid (e.g., empty input).
+      if (this.searchForm.invalid) {
+        alert('Invalid search form data. Please enter a name to search.');
         return;
       }
+
+      // Get the name entered in the search form.
+      const nameToSearch = this.searchForm.get('name')?.value;
+
+      // Filter the shopList based on the name input.
+      this.filteredShopList = this.shopList.filter(
+        shop => shop.name.toLowerCase().trim().includes(nameToSearch.toLowerCase().trim())
+      );
+
+      // Check if there are no results after filtering.
+      if (this.filteredShopList.length === 0) {
+        alert('No shops found for the entered name.');
+      }
+    }
+
+    resetSearch(): void {
+      // Method to reset the search and show all shops again.
+
+      // Reset the search form to clear the entered name.
+      this.searchForm.reset();
+
+      // Reset the filteredShopList to show all shops again by copying shopList.
+      this.filteredShopList = [...this.shopList];
     }
   }
-
-  searchByName(): void {
-    // Method to search for shops by name and display the results.
-
-    // Checking if the searchForm data is valid
-    if (this.searchForm.invalid) {
-      alert('Invalid search form data. Please enter a name to search.');
-      return;
-    }
-
-    // Extracting the name to search from the searchForm
-    const nameToSearch = this.searchForm.value.name;
-
-    // Filtering the shopList based on the name input and updating the filteredShopList
-    this.filteredShopList = this.shopList.filter(
-      shop => shop.name.toLowerCase().trim().includes(nameToSearch.toLowerCase().trim())
-    );
-
-    // Check if there are no results after filtering
-    if (this.filteredShopList.length === 0) {
-      alert('No shops found for the entered name.');
-    }
-  }
-
-  resetSearch(): void {
-    // Method to reset the search and show all shops again
-
-    // Resetting the search form
-    this.searchForm.reset();
-
-    // Resetting the filteredShopList to display all shops again
-    this.filteredShopList = [...this.shopList];
-  }
-}
